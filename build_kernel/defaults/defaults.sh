c05764f30c70ae5c557eda7722df17af93e33d9c
UpInTheAir <upintheair.xda@gmail.com>
2016-10-21 17:29:12 +0700

    initial setup & import from existing SkyHigh build environment
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

---
+++ build_kernel/defaults/defaults.sh
@@ -0,0 +1,159 @@
+#!/sbin/sh
+
+# Some elements & ideas from g.lewarne
+# by UpInTheAir for SkyHigh kernels
+
+# DETECT
+if [ "$1" = "DETECT" ]; then
+	if [ "$(grep "N920C" /proc/cmdline)" != "" ] || [ "$(grep "N920CD" /proc/cmdline)" != "" ] || [ "$(grep "N920G" /proc/cmdline)" != "" ] || [ "$(grep "N920I" /proc/cmdline)" != "" ] || [ "$(grep "N920P" /proc/cmdline)" != "" ] || [ "$(grep "N920T" /proc/cmdline)" != "" ] || [ "$(grep "N920W8" /proc/cmdline)" != "" ] || [ "$(grep "N9200" /proc/cmdline)" != "" ] || [ "$(grep "N9208" /proc/cmdline)" != "" ] || [ "$(grep "G928C" /proc/cmdline)" != "" ] || [ "$(grep "G928F" /proc/cmdline)" != "" ] || [ "$(grep "G928G" /proc/cmdline)" != "" ] || [ "$(grep "G928I" /proc/cmdline)" != "" ] || [ "$(grep "G9287C" /proc/cmdline)" != "" ] || [ "$(grep "G928P" /proc/cmdline)" != "" ] || [ "$(grep "G928T" /proc/cmdline)" != "" ] || [ "$(grep "G928W8" /proc/cmdline)" != "" ]; then
+		echo "device.type=compatible" >> /system/device.prop
+	else
+		echo "device.type=incompatible" >> /system/device.prop
+	fi;
+fi;
+
+# SELINUX
+if [ "$1" = "PERMISSIVE" ]; then
+	echo "selinux_permissive" >> /system/SkyHigh.prop
+elif [ "$1" = "ENFORCING" ]; then
+	echo "selinux_enforcing" >> /system/SkyHigh.prop
+fi;
+
+# BIND
+if [ "$1" = "BIND" ]; then
+	if [ -f /data/.supersu ]; then
+		rm -rf /data/.supersu;
+		echo BINDSYSTEMXBIN=true >> /data/.supersu;
+	elif [ ! -f /data/.supersu ]; then
+		echo BINDSYSTEMXBIN=true >> /data/.supersu;
+	fi;
+fi;
+
+# INVISIBLE CPUSETS
+if [ "$1" = "INVISIBLE" ]; then
+	if [ -f /data/.SkyHigh/invisible_cpuset ]; then
+		rm -rf /data/.SkyHigh/invisible_cpuset;
+	fi;
+	echo 1 >> /data/.SkyHigh/invisible_cpuset;
+fi;
+
+# LAZYTIME MOUNT
+if [ "$1" = "LAZYTIME" ]; then
+	if [ -f /data/.SkyHigh/lazytime_mount ]; then
+		rm -rf /data/.SkyHigh/lazytime_mount;
+	fi;
+	echo 1 >> /data/.SkyHigh/lazytime_mount;
+fi;
+
+# BUILD.PROP
+if [ "$1" = "BUILD_PROP" ]; then
+	# backup original build.prop
+	if [ ! -f /system/build.prop.orig ]; then
+		cp /system/build.prop /system/build.prop.orig
+	fi;
+
+	# check multi-user
+	if [ "$(grep "fw.max_users" /system/build.prop)" != "" ]; then
+		max_users="fw.max_users=30";
+	fi;
+	if [ "$(grep "fw.show_multiuserui" /system/build.prop)" != "" ]; then
+		show_multiuserui="fw.show_multiuserui=1";
+	fi;
+	if [ "$(grep "fw.show_hidden_users" /system/build.prop)" != "" ]; then
+		show_hidden_users="fw.show_hidden_users=1";
+	fi;
+	if [ "$(grep "fw.power_user_switcher" /system/build.prop)" != "" ]; then
+		power_user_switcher="fw.power_user_switcher=1";
+	fi;
+
+	# check Viper4Android
+	if [ "$(grep "lpa.decode" /system/build.prop)" != "" ]; then
+		decode="lpa.decode=false";
+	fi;
+	if [ "$(grep "lpa.releaselock" /system/build.prop)" != "" ]; then
+		releaselock="lpa.releaselock=false";
+	fi;
+	if [ "$(grep "lpa.use-stagefright" /system/build.prop)" != "" ]; then
+		stagefright="lpa.use-stagefright=false";
+	fi;
+	if [ "$(grep "tunnel.decode" /system/build.prop)" != "" ]; then
+		tunnel="tunnel.decode=false";
+	fi;
+
+	# remove placebo & bullshit 'tweaks'
+	sed -i '/ro.expect.recovery_id/q' /system/build.prop;
+
+	# stock kernel
+	echo "#" >> /system/build.prop
+	echo "# Keep WIFI settings on flash/reboot" >> /system/build.prop
+	echo "ro.securestorage.support=false" >> /system/build.prop
+	echo "# Screen mirroring / AllShare Cast fix" >> /system/build.prop
+	echo "wlan.wfd.hdcp=disable" >> /system/build.prop
+	echo "#" >> /system/build.prop
+
+	# multi-user
+	if [ "$max_users" == "fw.max_users=30" ]; then
+		echo "fw.max_users=30" >> /system/build.prop
+	fi;
+	if [ "$show_multiuserui" == "fw.show_multiuserui=1" ]; then
+		echo "fw.show_multiuserui=1" >> /system/build.prop
+	fi;
+	if [ "$show_hidden_users" == "fw.show_hidden_users=1" ]; then
+		echo "fw.show_hidden_users=1" >> /system/build.prop
+	fi;
+	if [ "$power_user_switcher" == "fw.power_user_switcher=1" ]; then
+		echo "fw.power_user_switcher=1" >> /system/build.prop
+	fi;
+
+	# Viper4Android
+	if [ "$decode" == "lpa.decode=false" ]; then
+		echo "lpa.decode=false" >> /system/build.prop
+	fi;
+	if [ "$releaselock" == "lpa.releaselock=false" ]; then
+		echo "lpa.releaselock=false" >> /system/build.prop
+	fi;
+	if [ "$stagefright" == "lpa.use-stagefright=false" ]; then
+		echo "lpa.use-stagefright=false" >> /system/build.prop
+	fi;
+	if [ "$tunnel" == "tunnel.decode=false" ]; then
+		echo "tunnel.decode=false" >> /system/build.prop
+	fi;
+
+	chmod 644 /system/build.prop;
+fi;
+
+# CPU GOVERNOR
+if [ "$1" = "INTERACTIVE" ]; then
+	echo "cpugov_interactive" >> /system/SkyHigh.prop
+elif [ "$1" = "KTOONSERVATIVE" ]; then
+	echo "cpugov_ktoonservative" >> /system/SkyHigh.prop
+elif [ "$1" = "PERFORMANCE" ]; then
+	echo "cpugov_performance" >> /system/SkyHigh.prop
+fi;
+
+# IO SCHEDULER
+if [ "$1" = "ROW" ]; then
+	echo "sched_row" >> /system/SkyHigh.prop
+elif [ "$1" = "CFQ" ]; then
+	echo "sched_cfq" >> /system/SkyHigh.prop
+elif [ "$1" = "FIOPS" ]; then
+	echo "sched_fiops" >> /system/SkyHigh.prop
+elif [ "$1" = "NOOP" ]; then
+	echo "sched_noop" >> /system/SkyHigh.prop
+elif [ "$1" = "DEADLINE" ]; then
+	echo "sched_deadline" >> /system/SkyHigh.prop
+elif [ "$1" = "BFQ" ]; then
+	echo "sched_bfq" >> /system/SkyHigh.prop
+elif [ "$1" = "SIOPLUS" ]; then
+	echo "sched_sioplus" >> /system/SkyHigh.prop
+fi;
+
+# CORTEX
+if [ "$1" = "CORTEX_ENABLED" ]; then
+	echo "cortex_enabled" >> /system/SkyHigh.prop
+fi;
+
+# CRONTAB
+if [ "$1" = "CRONTAB_ENABLED" ]; then
+	echo "crontab_enabled" >> /system/SkyHigh.prop
+fi;
---
+++ build_kernel/sbin/SkyHigh.sh
@@ -0,0 +1,241 @@
+#!/system/bin/sh
+
+BB=/system/xbin/busybox;
+
+# Mount root as RW to apply tweaks and settings
+if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
+	$BB mount -o remount,rw /;
+fi;
+if [ "$($BB mount | grep system | grep -c ro)" -eq "1" ]; then
+	$BB mount -o remount,rw /system;
+fi;
+
+
+# Variant detection for supported devices
+if [ "$(grep "N920C" /proc/cmdline)" != "" ] || [ "$(grep "N920CD" /proc/cmdline)" != "" ] || [ "$(grep "N920G" /proc/cmdline)" != "" ] || [ "$(grep "N920I" /proc/cmdline)" != "" ] || [ "$(grep "N920P" /proc/cmdline)" != "" ] || [ "$(grep "N920T" /proc/cmdline)" != "" ] || [ "$(grep "N920W8" /proc/cmdline)" != "" ] || [ "$(grep "N9200" /proc/cmdline)" != "" ] || [ "$(grep "N9208" /proc/cmdline)" != "" ] || [ "$(grep "G928C" /proc/cmdline)" != "" ] || [ "$(grep "G928F" /proc/cmdline)" != "" ] || [ "$(grep "G928G" /proc/cmdline)" != "" ] || [ "$(grep "G928I" /proc/cmdline)" != "" ] || [ "$(grep "G9287C" /proc/cmdline)" != "" ] || [ "$(grep "G928P" /proc/cmdline)" != "" ] || [ "$(grep "G928T" /proc/cmdline)" != "" ] || [ "$(grep "G928W8" /proc/cmdline)" != "" ]; then
+
+
+	# Set SELinux permissive by default (parse defaults from prop)
+	if [ "$(grep "selinux_permissive" /system/SkyHigh.prop)" != "" ]; then
+		setenforce 0
+	elif [ "$(grep "selinux_enforcing" /system/SkyHigh.prop)" != "" ]; then
+		setenforce 1
+	fi;
+
+
+	# Make directory for Cron Task & cpuset
+	if [ ! -d /data/.SkyHigh ]; then
+		$BB mkdir -p /data/.SkyHigh
+		$BB chmod -R 0777 /.SkyHigh/
+	fi;
+
+
+	# Setup for cpuset
+	if [ -d /dev/cpuset ]; then
+		echo "0-7" > /dev/cpuset/foreground/cpus;
+		echo "4-7" > /dev/cpuset/foreground/boost/cpus;
+		echo "0" > /dev/cpuset/background/cpus;
+		echo "0-3" > /dev/cpuset/system-background/cpus;
+		# Move invisible apps to A53
+		if [ -d /dev/cpuset/invisible ]; then
+			echo "0-3" > /dev/cpuset/invisible/cpus;
+		fi;
+	fi;
+
+
+	# Backup EFS
+	if [ ! -d /data/media/0/SkyHigh/Synapse/EFS ]; then
+		$BB mkdir -p /data/media/0/SkyHigh/Synapse/EFS;
+	fi;
+	if [ ! -e /data/media/0/SkyHigh/Synapse/EFS/efs_backup.img ]; then
+		$BB dd if=dev/block/platform/15570000.ufs/by-name/EFS of=/data/media/0/SkyHigh/Synapse/EFS/efs_backup.img 2> /dev/null;
+	fi;
+
+
+	# Reset CortexBrain WiFi auto screen ON-OFF intervals
+	if [ -e /data/.wifi_scron.log ]; then
+		rm /data/.wifi_scron.log;
+	fi;
+	if [ -e /data/.wifi_scroff.log ]; then
+		rm /data/.wifi_scroff.log;
+	fi;
+
+
+	# Set correct r/w permissions for LMK parameters
+	$BB chmod 666 /sys/module/lowmemorykiller/parameters/cost;
+	$BB chmod 666 /sys/module/lowmemorykiller/parameters/adj;
+	$BB chmod 666 /sys/module/lowmemorykiller/parameters/minfree;
+
+
+	# Disable rotational storage for all blocks
+	# We need faster I/O so do not try to force moving to other CPU cores (dorimanx)
+	for i in /sys/block/*/queue; do
+		echo "0" > "$i"/rotational;
+		echo "2" > "$i"/rq_affinity;
+	done;
+
+
+	# Allow untrusted apps to read from debugfs (mitigate SELinux denials)
+	if [ -e /su/lib/libsupol.so ]; then
+	/system/xbin/supolicy --live \
+		"allow untrusted_app debugfs file { open read getattr }" \
+		"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
+		"allow untrusted_app sysfs_devices_system_iosched file { open read getattr }" \
+		"allow untrusted_app persist_file dir { open read getattr }" \
+		"allow debuggerd gpu_device chr_file { open read getattr }" \
+		"allow netd netd capability fsetid" \
+		"allow netd { hostapd dnsmasq } process fork" \
+		"allow { system_app shell } dalvikcache_data_file file write" \
+		"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
+		"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
+		"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
+		"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
+		"allow system_server dex2oat_exec file rx_file_perms" \
+		"allow mediaserver mediaserver_tmpfs file execute" \
+		"allow drmserver theme_data_file file r_file_perms" \
+		"allow zygote system_file file write" \
+		"allow atfwd property_socket sock_file write" \
+		"allow untrusted_app sysfs_display file { open read write getattr add_name setattr remove_name }" \
+		"allow debuggerd app_data_file dir search" \
+		"allow sensors diag_device chr_file { read write open ioctl }" \
+		"allow sensors sensors capability net_raw" \
+		"allow init kernel security setenforce" \
+		"allow netmgrd netmgrd netlink_xfrm_socket nlmsg_write" \
+		"allow netmgrd netmgrd socket { read write open ioctl }"
+	fi;
+
+
+	if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
+		$BB mount -o remount,rw /;
+	fi;
+
+
+	# Fix for earphone / handsfree no in-call audio
+	if [ -d "/sys/class/misc/arizona_control" ]; then
+		echo "1" >/sys/class/misc/arizona_control/switch_eq_hp
+	fi;
+
+
+	# CPU governor (parse defaults from prop)
+	if [ "$(grep "cpugov_interactive" /system/SkyHigh.prop)" != "" ]; then
+		echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+		echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor;
+	elif [ "$(grep "cpugov_ktoonservative" /system/SkyHigh.prop)" != "" ]; then
+		echo "ktoonservative" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+		echo "ktoonservative" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor;
+	elif [ "$(grep "cpugov_performance" /system/SkyHigh.prop)" != "" ]; then
+		echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+		echo "performance" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor;
+	fi;
+
+
+	# IO scheduler (parse defaults from prop)
+	if [ "$(grep "sched_row" /system/SkyHigh.prop)" != "" ]; then
+		for i in /sys/block/sd*/queue/scheduler; do
+			$BB echo "row" > "$i" 2> /dev/null;
+		done;
+	elif [ "$(grep "sched_cfq" /system/SkyHigh.prop)" != "" ]; then
+		for i in /sys/block/sd*/queue/scheduler; do
+			$BB echo "cfq" > "$i" 2> /dev/null;
+		done;
+	elif [ "$(grep "sched_fiops" /system/SkyHigh.prop)" != "" ]; then
+		for i in /sys/block/sd*/queue/scheduler; do
+			$BB echo "fiops" > "$i" 2> /dev/null;
+		done;
+	elif [ "$(grep "sched_noop" /system/SkyHigh.prop)" != "" ]; then
+		for i in /sys/block/sd*/queue/scheduler; do
+			$BB echo "noop" > "$i" 2> /dev/null;
+		done;
+	elif [ "$(grep "sched_bfq" /system/SkyHigh.prop)" != "" ]; then
+		for i in /sys/block/sd*/queue/scheduler; do
+			$BB echo "bfq" > "$i" 2> /dev/null;
+		done;
+	elif [ "$(grep "sched_sioplus" /system/SkyHigh.prop)" != "" ]; then
+		for i in /sys/block/sd*/queue/scheduler; do
+			$BB echo "sioplus" > "$i" 2> /dev/null;
+		done;
+#	elif [ "$(grep "sched_deadline" /system/SkyHigh.prop)" != "" ]; then
+#		for i in /sys/block/sd*/queue/scheduler; do
+#			$BB echo "deadline" > "$i" 2> /dev/null;
+#		done;
+	fi;
+
+
+	# CortexBrain (parse defaults from prop)
+	if [ "$(grep "cortex_enabled" /system/SkyHigh.prop)" != "" ]; then
+		echo "1" > /res/synapse/SkyHigh/cortexbrain_background_process;
+	else
+		echo "0" > /res/synapse/SkyHigh/cortexbrain_background_process;
+	fi;
+
+
+	# Crontab (parse defaults from prop)
+	if [ "$(grep "crontab_enabled" /system/SkyHigh.prop)" != "" ]; then
+		echo "1" > /res/synapse/SkyHigh/cron/master;
+	else
+		echo "0" > /res/synapse/SkyHigh/cron/master;
+	fi;
+
+
+	# Synapse
+	$BB chmod -R 755 /res/*
+	$BB ln -fs /res/synapse/uci /sbin/uci
+	/sbin/uci
+
+
+	if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
+		$BB mount -o remount,rw /;
+	fi;
+	if [ "$($BB mount | grep system | grep -c ro)" -eq "1" ]; then
+		$BB mount -o remount,rw /system;
+	fi;
+
+
+	# Init.d
+	if [ ! -d /system/etc/init.d ]; then
+		mkdir -p /system/etc/init.d/;
+		chown -R root.root /system/etc/init.d;
+		chmod 777 /system/etc/init.d/;
+		chmod 777 /system/etc/init.d/*;
+	fi;
+	$BB run-parts /system/etc/init.d
+
+
+	# Arizona earphone sound default (parametric equalizer preset values by AndreiLux)
+	if [ -d "/sys/class/misc/arizona_control" ]; then
+		sleep 20;
+		echo "0x0FF3 0x041E 0x0034 0x1FC8 0xF035 0x040D 0x00D2 0x1F6B 0xF084 0x0409 0x020B 0x1EB8 0xF104 0x0409 0x0406 0x0E08 0x0782 0x2ED8" > /sys/class/misc/arizona_control/eq_A_freqs
+		echo "0x0C47 0x03F5 0x0EE4 0x1D04 0xF1F7 0x040B 0x07C8 0x187D 0xF3B9 0x040A 0x0EBE 0x0C9E 0xF6C3 0x040A 0x1AC7 0xFBB6 0x0400 0x2ED8" > /sys/class/misc/arizona_control/eq_B_freqs
+	fi;
+
+
+	# Run Cortexbrain script
+	# Cortex parent should be ROOT/INIT and not Synapse
+	cortexbrain_background_process=$(cat /res/synapse/SkyHigh/cortexbrain_background_process);
+	if [ "$cortexbrain_background_process" == "1" ]; then
+		sleep 30
+		$BB nohup $BB sh /sbin/cortexbrain-tune.sh > /dev/null 2>&1 &
+	fi;
+
+
+	# Start CROND by tree root, so it's will not be terminated.
+	cron_master=$(cat /res/synapse/SkyHigh/cron/master);
+	if [ "$cron_master" == "1" ]; then
+		$BB nohup $BB sh /res/crontab_service/service.sh 2> /dev/null;
+	fi;
+
+
+	# Kernel custom test
+	if [ -e /data/.SkyHigh_test.log ]; then
+		rm /data/.SkyHigh_test.log
+	fi;
+	echo  Kernel script is working !!! >> /data/.SkyHigh_test.log
+	echo "excecuted on $(date +"%d-%m-%Y %r" )" >> /data/.SkyHigh_test.log
+
+
+fi;
+
+
+$BB mount -t rootfs -o remount,ro rootfs
+$BB mount -o remount,ro /system
+$BB mount -o remount,rw /data
